2586c4a376185fc6603bda1b5e1e640dc7586249
Yixa <1257597350@qq.com>
2023-11-09 10:54:52 +0800

    wIp: add cycle member in struct Instruction

--- src/tomasulo-sim/instruction.rs
+++ src/tomasulo-sim/instruction.rs
@@ -13,7 +13,15 @@ pub enum Type {
 pub struct Instruction{
     pub op:Type,
 
-    pub issue_cycle: u8,
+    // when the instruction issued,
+    // it may waitting for some value to execute
+    // and has no these cycle_values,
+    // so we use Option<>.
+    pub issue_cycle: Option<u8>,
+    pub execute_begin_cycle: Option<u8>,
+    pub execute_end_cycle: Option<u8>,
+    pub write_back_cycle: Option<u8>,
+    pub commit_cycle: Option<u8>,
     
     
 }
